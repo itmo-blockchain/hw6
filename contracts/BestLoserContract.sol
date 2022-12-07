47e8a6355bd1de448f0977740ea1a5d95735596d
Michael Ruzavin <misharuz25@gmail.com>
2022-12-07 04:40:24 +0300

    Make pretty output

--- contracts/BestLoserContract.sol
+++ contracts/BestLoserContract.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.0;
 
 import { FlashLoanReceiverBase } from "@aave/protocol-v2/contracts/flashloan/base/FlashLoanReceiverBase.sol";
@@ -8,20 +9,18 @@ import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 contract BestLoserContract is FlashLoanReceiverBase, Ownable {
 
     address public router;
-
     address[] public path;
 
     constructor(address _addressProvider, address _router, address[] memory _path) FlashLoanReceiverBase(ILendingPoolAddressesProvider(_addressProvider)) public {
-        router = _router;
-
-        require(_path.length >= 3, "Path must have at least 2 elements and last element must be equal to the first");
+        require(_path.length >= 3, "Path must have at least 3 elements");
         require(_path[0] == _path[_path.length - 1], "Path must start and end with the same token");
 
+        router = _router;
         path = _path;
     }
 
@@ -29,33 +28,18 @@ contract BestLoserContract is FlashLoanReceiverBase, Ownable {
         address[] calldata assets,
         uint256[] calldata amounts,
         uint256[] calldata premiums,
-        address initiator,
-        bytes calldata params
+        address,
+        bytes calldata
     ) external override returns (bool) {
-        // Do your thing here.
-        // The contract has the requested _amount of _reserve available as balance in the address of this contract.
-        // The _fee of the _amount needs to be repaid.
-        // The _params variable is arbitrary and can be used to pass additional variables to this function.
-        // This contract must have the required approval to *pull* the _amount of _reserve.
-        // The contract must have the required approval to *push* the _fee of _amount of _reserve.
 
-        // Swap the _amount of _reserve for the token in the path
-
-
+        require(assets.length == 1 && amounts.length == 1 && premiums.length == 1, "Invalid arrays length");
+        require(assets[0] == path[0], "Invalid asset");
 
         SafeERC20.safeApprove(IERC20(assets[0]), router, amounts[0]);
 
         IUniswapV2Router02(router).swapExactTokensForTokens(amounts[0], 0, path, address(this), block.timestamp);
-
-        // Approve the LendingPool contract allowance to *pull* the owed amount
-        uint balance = IERC20(assets[0]).balanceOf(address(this));
-        console.log("Approving LendingPool contract allowance to *pull* the owed amount");
         
         uint totalDebt = amounts[0] + premiums[0];
-
-        console.log("Balance:    %s", balance);
-        console.log("Total debt: %s", totalDebt);
-
         SafeERC20.safeApprove(IERC20(assets[0]), address(LENDING_POOL), totalDebt);
 
         return true;
@@ -73,8 +57,6 @@ contract BestLoserContract is FlashLoanReceiverBase, Ownable {
 
         uint256[] memory modes = new uint256[](1);
         modes[0] = 0;
-
-        console.log("Calling flash loan");
 
         LENDING_POOL.flashLoan(receiverAddress, assets, amounts, modes, address(0), "", 0);
     }
--- test/BestLoserContract.ts
+++ test/BestLoserContract.ts
@@ -10,6 +10,10 @@ const WETH_ADDRESS = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 const DAI_ADDRESS = "0x6b175474e89094c44da98b954eedeac495271d0f"
 const WBTC_ADDRESS = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
 
+const DEPLOYED_ORDER = [
+    WETH_ADDRESS, DAI_ADDRESS, WBTC_ADDRESS, WETH_ADDRESS
+]
+
 const SWAP_EVENT_ABI = [
     "event Swap(address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to)"
 ]
@@ -23,7 +27,7 @@ describe("BestLoserContract", function () {
         const bestLoserContract = await BestLoserContract.deploy(
             PROVIDER_ADDRESS,
             UNISWAP_V2_ROUTER_ADDRESS, 
-            [WETH_ADDRESS, DAI_ADDRESS, WBTC_ADDRESS, WETH_ADDRESS]
+            DEPLOYED_ORDER
         ); 
         
         console.log("BestLoserContract deployed to:", bestLoserContract.address);
@@ -40,20 +44,46 @@ describe("BestLoserContract", function () {
         const {bestLoserContract, weth, owner} = await loadFixture(deployBestLoserFixture);
 
         weth.connect(owner).transfer(bestLoserContract.address, ethers.utils.parseEther("1"))
-        console.log(await weth.balanceOf(bestLoserContract.address))
 
+        const balanceBefore = await weth.balanceOf(bestLoserContract.address)
+        console.log("Balance of BestLoserContract: " + balanceBefore.toString() + " WGwei \n")
+        
         const tx = await bestLoserContract.flashBorrow(ethers.utils.parseEther("1"))
-
+        
         const receipt = await tx.wait()
-
-        console.log(receipt.logs.map(log => {
+        
+        receipt.logs.map(log => {
             try {
                 return SWAP_EVENT_TOPIC.parseLog(log)
             } catch (e) {
                 return undefined
             }
-        }).filter(log => log !== undefined))
+        }).filter(log => log !== undefined).forEach((log, index) => {
+            let [fst, snd] = [DEPLOYED_ORDER[index], DEPLOYED_ORDER[index + 1]];
+            
+            if (fst > snd) {
+                [fst, snd] = [snd, fst]
+            }
 
-        console.log(await weth.balanceOf(bestLoserContract.address))
+            console.log(
+                "Swap event:\n\tToken0: %s -> In: %s, Out: %s\n\tToken1: %s -> In: %s, Out: %s",
+                fst,
+                log?.args.amount0In.toString(),
+                log?.args.amount0Out.toString(),
+                snd,
+                log?.args.amount1In.toString(),
+                log?.args.amount1Out.toString()
+            )
+        })
+        console.log()
+        
+        const balanceAfter = await weth.balanceOf(bestLoserContract.address)
+        console.log("Balance of BestLoserContract: " + balanceAfter.toString() + " WGwei\n")
+
+        if (balanceBefore > balanceAfter) {
+            console.log("Profit: " + (balanceAfter.sub(balanceBefore)).toString() + " WGwei")
+        } else {
+            console.log("Loss: " + (balanceBefore.sub(balanceAfter)).toString() + " WGwei")
+        }
     });
 });
